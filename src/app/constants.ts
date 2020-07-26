f92594e3b65a2888015d48ca7c57a7ba5f0a42d5
Khalil Maaref <khalil.maaref@gmail.com>
2020-07-26 20:02:11 +0100

    http

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 
 export const BASE_URL = "https://khalilmaaref.pythonanywhere.com/";
-export const BASE_URL_SERVER_RP = "https://51.178.46.228:5000/";
+export const BASE_URL_SERVER_RP = "http://51.178.46.228:5000/";
